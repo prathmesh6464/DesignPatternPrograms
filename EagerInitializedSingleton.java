d4950afd9f43ee96eb7a8b474b10e3b59d287b93
prathamesh <prathmesh6464@gmail.com>
2020-03-16 18:07:50 +0530

    ADDED : Eager initialized singleton.

---
+++ EagerInitializedSingleton.java
@@ -0,0 +1,39 @@
+package EagerInitializedSingleton;
+
+
+class EagerIntialization
+{
+	//SATIC VARIABLE
+	static int objectCount = 0;
+	
+	
+	//ONLY ONE OBJECT CREATED
+	private static final EagerIntialization onlyOneInstance = new EagerIntialization();
+
+
+	//private constructor to avoid client applications to use constructor
+	private EagerIntialization()
+	{
+		objectCount++;
+	}
+
+
+	//RETURN OBJECT
+	public static EagerIntialization getInstance()
+	{
+		return onlyOneInstance;		
+	}
+}
+
+
+//CLASS EAGERINITIALIZEDSINGLETON
+class EagerInitializedSingleton 
+{
+	//MAIN METHOD
+	public static void main(String[] args) 
+	{
+		EagerIntialization firstObjectReference = EagerIntialization.getInstance();
+		EagerIntialization firstObjectReference2 = EagerIntialization.getInstance();
+		System.out.println(firstObjectReference2.objectCount);
+	}
+}  
