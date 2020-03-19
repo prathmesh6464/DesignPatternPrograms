737c05a23053c29da9d7ca92cf0e7dc2ffcc1ce8
Prathamaesh <prathmesh6464@gmail.com>
2020-03-19 16:01:43 +0530

    REFACTORED : Added main method.

--- BillPughSingletonImplementation/BillPughSingletonImplementation.java
+++ BillPughSingletonImplementation/BillPughSingletonImplementation.java
@@ -1,25 +1,49 @@
 package BillPughSingletonImplementation;
 
 
-public class BillPughSingletonImplementation 
+//BILL PUGH SINGLETON CLASS
+class BillPughSingleton 
 {
+	//VARIABLE
+	static int temparoryCheck = 0;
+	
+	
 	//CONSTRUCTOR WILL BE CALLED ONLY ONCE
-	private BillPughSingletonImplementation()
+	private BillPughSingleton()
 	{
-		System.out.println("Constructor");
+		temparoryCheck++;
+		System.out.println(temparoryCheck);
 	}
 
 
 	//NO NEED OF SYNCHRONIZATION
 	private static class SingletonHelper
 	{
-		private static final BillPughSingletonImplementation INSTANCE = new BillPughSingletonImplementation();
+		private static final BillPughSingleton INSTANCE = new BillPughSingleton();
 	}
 
 	
 	//RETURNING OBJECT
-	public static BillPughSingletonImplementation getInstance()
+	public static BillPughSingleton getInstance()
 	{
 		return SingletonHelper.INSTANCE;
+	}
+}
+
+
+//BILL PUSH SINGLETON IMPLEMENTATION CLASS
+public class BillPughSingletonImplementation
+{
+	//MAIN CLASS
+	public static void main(String[] args)
+	{
+		//CREATING FIRST OBJECT REFERENCE
+		BillPughSingleton billPughSingletonObject = BillPughSingleton.getInstance();
+		billPughSingletonObject.getInstance();
+		
+		
+		//CREATING SECOND OBJECT REFERENCE
+		BillPughSingleton billPughSingletonObject2 = BillPughSingleton.getInstance();
+		billPughSingletonObject.getInstance();	
 	}
 }
