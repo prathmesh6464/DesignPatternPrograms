fae59277d2098556602f5daf01d480aa89dd1ebc
Prathamaesh <prathmesh6464@gmail.com>
2020-03-20 00:09:41 +0530

    ADDED : Employees class.

--- PrototypeDesignPattern/PrototypeDesignPattern.java
+++ PrototypeDesignPattern/PrototypeDesignPattern.java
@@ -1,6 +1,4 @@
 package PrototypeDesignPattern;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,32 +6,8 @@ class Employees implements Cloneable
 {
 	private List<String> empList;
 	
-	public Employees(){
+	public Employees()
+	{
 		empList = new ArrayList<String>();
 	}
-	
-	public Employees(List<String> list){
-		this.empList=list;
-	}
-	public void loadData(){
-		//read all employees from database and put into the list
-		empList.add("Pankaj");
-		empList.add("Raj");
-		empList.add("David");
-		empList.add("Lisa");
-	}
-	
-	public List<String> getEmpList() {
-		return empList;
-	}
-
-	@Override
-	public Object clone() throws CloneNotSupportedException{
-			List<String> temp = new ArrayList<String>();
-			for(String s : this.getEmpList()){
-				temp.add(s);
-			}
-			return new Employees(temp);
-	}
-	
 }
