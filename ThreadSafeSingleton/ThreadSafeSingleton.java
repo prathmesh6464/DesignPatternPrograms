7b83db58f12f2d9dfadcbaaff0fc0ec20b37d1ef
Prathamaesh <prathmesh6464@gmail.com>
2020-03-19 17:08:43 +0530

    ADDED : main method and comments.

--- ThreadSafeSingleton/ThreadSafeSingleton.java
+++ ThreadSafeSingleton/ThreadSafeSingleton.java
@@ -1,25 +1,70 @@
 package ThreadSafeSingleton;
 
 
-class ThreadSafeSingleton 
+//THREAD SAFE CLASS
+class ThreadSafe implements Runnable
 {
+	int temparoryCheckCout = 0;
 	//DECLARING OBJECT REFERENCE
-	private static ThreadSafeSingleton instance;
+	private static ThreadSafe instance;
 
 
-	private ThreadSafeSingleton()
+	private ThreadSafe()
 	{
-		System.out.println("Constructor");
+		temparoryCheckCout++;
+		System.out.println(temparoryCheckCout);
 	}
 
 
 	//PROPER CREATION OF OBJECT  FOR EVERY SINGLE THREAD
-	public static synchronized ThreadSafeSingleton getInstance()
+	public static synchronized ThreadSafe getInstance()
 	{
 		if(instance == null)
 		{
-			instance = new ThreadSafeSingleton();
+			instance = new ThreadSafe();
 		}
 		return instance;
+	}
+
+
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+	}
+}
+
+
+//THREAD SAFE SINGLETON CLASS
+class ThreadSafeSingleton
+{
+	//MAIN METHOD
+	public static void main(String args[])
+	{
+		//FIRST THREAD
+		Thread firstThread = new Thread(new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				ThreadSafe firstObjectReference = ThreadSafe.getInstance();
+			}	
+		});
+		
+		
+		//SECOND THREAD
+		Thread secondThread = new Thread(new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				ThreadSafe secondObjectReference = ThreadSafe.getInstance();
+			}	
+		});
+		
+		
+		//STARTING THREAD
+		firstThread.start();
+		secondThread.start();
 	}
 }
