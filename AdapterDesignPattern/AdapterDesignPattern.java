4274f8357e279a19b383438a53e34e2800698334
Prathamaesh <prathmesh6464@gmail.com>
2020-03-20 22:31:48 +0530

    WIP : Added main method and socket,volt class.

--- AdapterDesignPattern/AdapterDesignPattern.java
+++ AdapterDesignPattern/AdapterDesignPattern.java
@@ -1,9 +1,42 @@
 package AdapterDesignPattern;
 
-public class AdapterDesignPattern {
-	public static void main(String args[]) {
-		
+
+class Volt 
+{
+	private int volts;
+	
+	public Volt(int v)
+	{
+		this.volts=v;
 	}
 
+	public int getVolts()
+	{
+		return volts;
+	}
+
+	public void setVolts(int volts)
+	{
+		this.volts = volts;
+	}	
 }
-  
+
+
+class Socket
+{
+
+	public Volt getVolt()
+	{
+		return new Volt(120);
+	}
+}
+
+
+public class AdapterDesignPattern 
+{
+	
+	public static void main(String args[]) 
+	{
+		  
+	}
+}
