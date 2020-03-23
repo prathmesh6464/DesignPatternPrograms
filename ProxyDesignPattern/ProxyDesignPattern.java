1ecc4c681f77fef82cf51fe324e13d1baa05f294
Prathamaesh <prathmesh6464@gmail.com>
2020-03-23 22:35:35 +0530

    MODIFIED : Added command executor implementation method.

--- ProxyDesignPattern/ProxyDesignPattern.java
+++ ProxyDesignPattern/ProxyDesignPattern.java
@@ -1,20 +1,32 @@
 package ProxyDesignPattern;
-
-
+import java.io.IOException;
 
 //INTERFACE COMMAND EXECUTOR
 interface CommandExecutor 
 {
-	public void runCommand(String cmd);
+	public void runCommand(String cmd) throws IOException;
+}
+
+
+//CLASS COMMAND EXECUTOR IMPLEMENTATION
+class CommandExecutorImplementation implements CommandExecutor
+{
+	@Override
+	public void runCommand(String cmd) throws IOException
+	{
+        Runtime.getRuntime().exec(cmd);
+		System.out.println("'" + cmd + "' command executed.");
+	}
 }
 
 
 //CLASS PROXY DESIGN PATTERN
 public class ProxyDesignPattern
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
-		// TODO Auto-generated method stub
-
+		CommandExecutorImplementation commandExecutorImplementationObject =  new CommandExecutorImplementation();
+		commandExecutorImplementationObject.runCommand("cmd");
+		commandExecutorImplementationObject.runCommand("cmd cd");
 	}
 }
