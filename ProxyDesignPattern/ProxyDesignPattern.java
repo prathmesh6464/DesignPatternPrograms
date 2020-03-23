4bf1f2bd63b5732848c0ffaf884e3479368484f0
Prathamaesh <prathmesh6464@gmail.com>
2020-03-24 00:17:54 +0530

    REFACTORED : Added command executor proxy.

--- ProxyDesignPattern/ProxyDesignPattern.java
+++ ProxyDesignPattern/ProxyDesignPattern.java
@@ -1,10 +1,11 @@
 package ProxyDesignPattern;
 import java.io.IOException;
 
+
 //INTERFACE COMMAND EXECUTOR
 interface CommandExecutor 
 {
-	public void runCommand(String cmd) throws IOException;
+	public void runCommand(String cmd) throws IOException, Exception;
 }
 
 
@@ -14,19 +15,63 @@ class CommandExecutorImplementation implements CommandExecutor
 	@Override
 	public void runCommand(String cmd) throws IOException
 	{
-        Runtime.getRuntime().exec(cmd);
+		Runtime.getRuntime().exec(cmd);
 		System.out.println("'" + cmd + "' command executed.");
 	}
 }
 
 
-//CLASS PROXY DESIGN PATTERN
-public class ProxyDesignPattern
+//COMMAND EXECUTOR PROXY 
+class CommandExecutorProxy implements CommandExecutor 
 {
-	public static void main(String[] args) throws IOException 
+	private boolean isAdmin;
+	private CommandExecutor executor;
+	
+	//CONSTRUCTOR
+	public CommandExecutorProxy(String userName, String password)
 	{
-		CommandExecutorImplementation commandExecutorImplementationObject =  new CommandExecutorImplementation();
-		commandExecutorImplementationObject.runCommand("cmd");
-		commandExecutorImplementationObject.runCommand("cmd cd");
+		if("Prathamesh".equals(userName) && "admin123".equals(password)) isAdmin = true;
+		executor = new CommandExecutorImplementation();
+	} 
+	
+	@Override
+	public void runCommand(String cmd) throws Exception 
+	{
+		if(isAdmin)
+		{
+			executor.runCommand(cmd);
+		}
+		else
+		{
+			if(cmd.trim().startsWith("rm"))
+			{
+				throw new Exception("rm command is not allowed for non-admin users.");
+			}
+			else
+			{
+				executor.runCommand(cmd);
+			}
+		}
 	}
 }
+
+
+//PROXY PATTERN TEST
+class ProxyDesignPattern 
+{
+	public static void main(String[] args)
+	{
+		CommandExecutor executor = new CommandExecutorProxy("Prathamesh", "adminSSS");
+		try 
+		{
+			executor.runCommand("ls -a");
+			executor.runCommand("   rm abc.pdf");
+		}
+		catch (Exception e) 
+		{
+			System.out.println("Exception Message : "+e.getMessage());
+		}		
+	}
+}
+
+
