a5b920c94dbf9429c55d0a97415d65fffe9797b8
Prathamaesh <prathmesh6464@gmail.com>
2020-03-24 23:26:07 +0530

    REFACTORED : Added my sql helper.

--- FacadePattern/FacadePattern.java
+++ FacadePattern/FacadePattern.java
@@ -1,10 +1,27 @@
 package FacadePattern;
+import java.sql.Connection;
 
-public class FacadePattern {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//CLASS MY SQL HELPER
+interface MySqlHelper
+{
+	public static Connection getMySqlDBConnection()
+	{
+		//get MySql DB connection using connection parameters
+		return null;
+	}
+	
+	void generateMySqlPDFReport(String tableName, Connection con);
+	
+	void generateMySqlHTMLReport(String tableName, Connection con);
+}
+
+public class FacadePattern
+{
+	//MAIN METHOD
+	public static void main(String[] args) 
+	{
+	
 
 	}
-
 }
