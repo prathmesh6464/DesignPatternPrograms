e90056329ed00136aac2837ef7a598a066d7402d
Prathamaesh <prathmesh6464@gmail.com>
2020-03-24 08:35:10 +0530

    RAFACTORED : Added subject interface and my topic class.

--- ObserverDesignPattern/ObserverDesignPattern.java
+++ ObserverDesignPattern/ObserverDesignPattern.java
@@ -1,25 +1,40 @@
 package ObserverDesignPattern;
 
 
+//OBSERVER INTERFACE
 interface Observer
 {
 	void update();
 	void setSubject();
 }
-interface subject
+
+
+//SUBJECT INTERFACE
+interface Subject
 {
 	void register(Observer observerOnTopic);
 	void unregister(Observer observerUnRegister);
 	void notifyObservers();
 	Object getUpdate();
 }
-class topic
-{
-	
-}
-public class ObserverDesignPattern {
 
-	public static void main(String[] args) {
+
+//CLASS MY TOPIC
+class MyTopic implements Observer,Subject 
+{
+	String message;
+	boolean changed;
+	Object MUTEX; 
+
+}
+
+
+//CLASS OBSSERVER DESIGN PATTERN 
+public class ObserverDesignPattern
+{
+	//MAIN METHOD
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
