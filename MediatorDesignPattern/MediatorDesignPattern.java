fc71f90dc6e3e24f2e641ff628da5a25c01e0b48
Prathamaesh <prathmesh6464@gmail.com>
2020-03-24 20:13:14 +0530

    MODIFIED : Changed and added comment of user class,main class and created object of user class.

--- MediatorDesignPattern/MediatorDesignPattern.java
+++ MediatorDesignPattern/MediatorDesignPattern.java
@@ -7,7 +7,7 @@ class ChatRoom
 {
 	public static void showMessage(User userName, String message)
 	{
-		System.out.println(new Date().toString()+" message Sent by : "+userName.getName()+"\n"+"\t"+message+"\n");
+		System.out.println(new Date().toString()+" message Sent by : "+userName.getName()+"\n"+"\t\t\t\t\t   --> "+message+"\n");
 	}
 }
 
@@ -16,19 +16,19 @@ class ChatRoom
 class User
 {
 	String userName;
-	
+
 	//GETTER
 	public String getName()
 	{
 		return userName;		
 	}
-	
+
 	//SETTER
 	public void setName(String name)
 	{
 		this.userName = name;
 	}
-	
+
 	public void sendMessage(String message)
 	{
 		ChatRoom.showMessage(this, message);
@@ -36,14 +36,24 @@ class User
 }
 
 
-//CLASS MEDIATORDESIGNPATTERN
+//CLASS MEDIATORDESIGNPATTERN ACTS AS A MEDIATER
 public class MediatorDesignPattern 
 {
 	//MAIN METHOD
 	public static void main(String[] args) 
 	{
-		
+		User userObject = new User();
 
+		//USER ONE
+		userObject.setName("Prathamesh");
+		userObject.sendMessage("Don't take it normally, situation is realy critical i think we should stay only at home");
+
+		//USER TWO
+		userObject.setName("Imran shaikh");
+		userObject.sendMessage("Are you talking about corona virus??");
+
+		//USER THREE
+		userObject.setName("Bridz labz");
+		userObject.sendMessage("Please come to office");
 	}
-
 }
