a3fa89d030a653a322d15c8150db0a97c058dbf3
Prathamaesh <prathmesh6464@gmail.com>
2020-03-24 08:26:58 +0530

    REFACTORED : Added interface Observer.

--- ObserverDesignPattern/ObserverDesignPattern.java
+++ ObserverDesignPattern/ObserverDesignPattern.java
@@ -1,5 +1,22 @@
 package ObserverDesignPattern;
 
+
+interface Observer
+{
+	void update();
+	void setSubject();
+}
+interface subject
+{
+	void register(Observer observerOnTopic);
+	void unregister(Observer observerUnRegister);
+	void notifyObservers();
+	Object getUpdate();
+}
+class topic
+{
+	
+}
 public class ObserverDesignPattern {
 
 	public static void main(String[] args) {
