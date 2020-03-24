3f17235a81663b7a06b34b18e4f0b48e5105c2f3
Prathamaesh <prathmesh6464@gmail.com>
2020-03-24 15:38:02 +0530

    REFACTORED : Added book,get price,git is book name number in class fruit.

--- VisitorDesignPattern/VisitorDesignPattern.java
+++ VisitorDesignPattern/VisitorDesignPattern.java
@@ -12,20 +12,20 @@ interface ItemElement
 class Book implements ItemElement
 {
 	private int price;
-	private String isbnNumber;
-	
-	public Book(int cost, String isbn)
+	private String isBookNameNumber;
+
+	public Book(int cost, String isBookNameNumber)
 	{
 		this.price = cost;
-		this.isbnNumber = isbn;
+		this.isBookNameNumber = isBookNameNumber;
 	}
-	
+
 	public int getPrice() {
 		return price;
 	}
 
-	public String getIsbnNumber() {
-		return isbnNumber;
+	public String getIsBookNameNumber() {
+		return isBookNameNumber;
 	}
 
 	@Override
@@ -39,13 +39,40 @@ class Book implements ItemElement
 //CLASS FRUIT
 class Fruit implements ItemElement
 {
+	private int pricePerKG;
+	private int weight;
+	private String name;
+
+	public Fruit(int pricePerKG, int weight, String name)
+	{
+		this.pricePerKG = pricePerKG;
+		this.weight = weight;
+		this.name = name;
+	}
+
+	public int getPricePerKg() 
+	{
+		return pricePerKG;
+	}
+
+
+	public int getWeight()
+	{
+		return weight;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
 
 	@Override
-	public int accept(ShopingCartVisitor shopingCartVisitorObject) {
-		// TODO Auto-generated method stub
-		return 0;
-	} 	
+	public int accept(ShopingCartVisitor shopingCartVisitorObject)
+	{
+		return shopingCartVisitorObject.visit(this);
+	}
 }
+
 
 
 
@@ -56,7 +83,7 @@ interface ShopingCartVisitor
 	int visit(Book bookObject);
 	int visit(Fruit fruitObject);
 }
- 
+
 
 
 //CLASS VISITOR DESIGN PATTERN
