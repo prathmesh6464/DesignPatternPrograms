e0f53936a1584f4b63ceb5c204b68509a6dff48b
Prathamaesh <prathmesh6464@gmail.com>
2020-03-27 10:30:54 +0530

    MODIFIED : Changed in get mysql db connection method in connection database class .

--- FacadePattern/FacadePattern.java
+++ FacadePattern/FacadePattern.java
@@ -8,79 +8,79 @@ import java.sql.Statement;
 //INTERFACE MY SQL HELPER
 interface MySqlHelper
 {
-	void getMySqlDbConnection() throws Exception;
+    void getMySqlDbConnection() throws Exception;
 }
 
 
 //INTERFACE ORACLE HELPER
 
-interface OracleHelper 
+interface OracleHelper
 {
-	void getOracleDbConnection() throws Exception;
+    void getOracleDbConnection() throws Exception;
 }
 
 
 //CLASS CONNECTION DATABASE ACTS AS A FACADE
 class ConnectionDatabase implements MySqlHelper,OracleHelper
 {
-	public void getMySqlDbConnection() throws Exception
-	{
-		String url = "jdbc:mysql://localhost:3306//FacadePatternDataBase";
-		String userName = "root";
-		String password = "";
-		String query = "select * from Student";
+    public void getMySqlDbConnection() throws Exception
+    {
+        String url = "jdbc:mysql://localhost:3306/TestDataBase";
+        String userName = "root";
+        String password = "admin";
+        String query = "select * from student";
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection connectionWithMysql = DriverManager.getConnection(url, userName, password);
-		Statement statement = connectionWithMysql.createStatement();
-		ResultSet resultSet = statement.executeQuery(query);
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connectionWithMysql = DriverManager.getConnection(url, userName, password);
+        Statement statement = connectionWithMysql.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
 
-		while(resultSet.next())
-		{
-			int studentRollNumber = resultSet.getInt(1);
-			String studentName = resultSet.getString(2);
-			System.out.println("Student roll number : "+studentRollNumber);
-			System.out.println("Student name : "+studentName);
-		}	
-		resultSet.close();
-		connectionWithMysql.close();
-	}
+        while(resultSet.next())
+        {
+            int studentRollNumber = resultSet.getInt(1);
+            String studentName = resultSet.getString(2);
+            System.out.println("Student roll number : "+studentRollNumber);
+            System.out.println("Student name : "+studentName);
+        }
+        resultSet.close();
+        connectionWithMysql.close();
+    }
 
-	public void getOracleDbConnection() throws Exception
-	{
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String userName = "system";
-		String password = "admin";
-		String query = "select * from customers";
+    public void getOracleDbConnection() throws Exception
+    {
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+        String userName = "system";
+        String password = "admin";
+        String query = "select * from customers";
 
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection connectionWithOracle = DriverManager.getConnection(url, userName, password);
-		Statement statement = connectionWithOracle.createStatement();
-		ResultSet resultSet = statement.executeQuery(query);
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection connectionWithOracle = DriverManager.getConnection(url, userName, password);
+        Statement statement = connectionWithOracle.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
 
-		while(resultSet.next())
-		{
-			int customerID = resultSet.getInt("customer_id");
-			String customerName = resultSet.getString("customer_name");
-			String customerCity = resultSet.getString("city");
-			System.out.println("Cutomer id : "+customerID);
-			System.out.println("Cutomer name : "+customerName);
-			System.out.println("Cutomer city : "+customerCity);				
-		}	
-		resultSet.close();
-		connectionWithOracle.close();
-	}
+        while(resultSet.next())
+        {
+            int customerID = resultSet.getInt("customer_id");
+            String customerName = resultSet.getString("customer_name");
+            String customerCity = resultSet.getString("city");
+            System.out.println("Cutomer id : "+customerID);
+            System.out.println("Cutomer name : "+customerName);
+            System.out.println("Cutomer city : "+customerCity);
+        }
+        resultSet.close();
+        connectionWithOracle.close();
+    }
 }
 
 
 //CLASS FACADE PATTERN
 public class FacadePattern
 {
-	//MAIN METHOD
-	public static void main(String[] args) throws Exception
-	{
-		ConnectionDatabase connectionDatabaseObject =  new ConnectionDatabase();
-		connectionDatabaseObject.getMySqlDbConnection();
-		connectionDatabaseObject.getOracleDbConnection();
-	}
+    //MAIN METHOD
+    public static void main(String[] args) throws Exception
+    {
+        ConnectionDatabase connectionDatabaseObject =  new ConnectionDatabase();
+        connectionDatabaseObject.getMySqlDbConnection();
+        connectionDatabaseObject.getOracleDbConnection();
+    }
 }
